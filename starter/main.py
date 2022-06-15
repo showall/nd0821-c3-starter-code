6f7d9e374bbf8341d4173a6d99c3531633bd5459
showall <gerardsho@gmail.com>
2022-06-15 20:56:00 +0800

    finalised

--- starter/main.py
+++ starter/main.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull -f") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
